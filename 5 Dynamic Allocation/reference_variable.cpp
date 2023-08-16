506fc9a42b9b9fb821f448bad7c0e4107384e70f
Satendra Kumar <satyendrakumar.contact@gmail.com>
2023-08-17 05:21:41 +0530

    addrd

---
+++ 5 Dynamic Allocation/reference_variable.cpp
@@ -0,0 +1,14 @@
+//creating reference created
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int i=5;
+
+    int &j = i;//reference variable created
+    cout<<j<<endl;
+
+    return 0;
+}
