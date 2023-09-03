190e32b66f093d1336b39fe7a9e725eda6b4c1f0
Satendra Kumar <satyendrakumar.contact@gmail.com>
2023-09-03 10:24:10 +0530

    sorting completed

--- 6 Recursion/sorting/quickSorting.cpp
+++ 6 Recursion/sorting/quickSorting.cpp
@@ -1,6 +1,41 @@
 #include<iostream>
 using namespace std;
 
+int partiton(int arr[], int s, int e){
+    
+    int pivot = arr[s];
+    int cnt = 0;
+
+    for(int i=s+1 ; i<=e ; i++){
+        if(arr[i] <= pivot){
+            cnt++;
+        }
+    }
+
+    //swaping to right position
+    int pivotIndex = s+cnt;
+    swap(arr[pivotIndex],arr[s]);
+
+    //sorting left and right part
+    int i=s,j=e;
+    while(i<pivotIndex && j>pivotIndex){
+        
+        while (arr[i]<arr[pivotIndex]){
+            i++;
+        }
+        while (arr[j]>arr[pivotIndex]){
+            j--;
+        }
+
+        if (i<pivotIndex && j>pivotIndex){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+
+}
+
 void quickSort(int arr[],int s, int e){
 
     //base case
@@ -8,8 +43,12 @@ void quickSort(int arr[],int s, int e){
         return ;
     }
 
-    //yoo
+    //partition process
+    int p = partiton(arr,s,e);
 
+    //recurive call
+    quickSort(arr,s,p-1);
+    quickSort(arr,p+1,e);
 }
 
 int main(){
@@ -18,7 +57,7 @@ int main(){
 
     for(int i=0;i<8;i++){
         cout<<arr[i]<<" ";
-    }
+    }cout<<endl;
     
     quickSort(arr,0,7);
 
